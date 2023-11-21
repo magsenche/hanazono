8fe19478f6a8efdf6a1698111b0626474aeaf611
Magsen Chebini <Magsen>
2023-11-21 21:51:37 +0100

    [dev] add css pbar

---
+++ src/plugin/pbar.py
@@ -0,0 +1,21 @@
+import re
+
+from bs4 import BeautifulSoup, Tag
+from mkdocs.plugins import BasePlugin
+
+
+class ProgressBarPlugin(BasePlugin):
+    def on_post_page(self, output_content, page, config):
+        soup = BeautifulSoup(output_content, "html.parser")
+        pbars = soup.find_all("a", {"class": "pbar"})
+        for pbar in pbars:
+            width, color = re.findall(r"(\d+)%,(\w+)", pbar.text)[0]
+            new_pbar = Tag(
+                name="div",
+                attrs={
+                    "class": "pbar",
+                    "style": f"width: {width}%; background-color: {color};",
+                },
+            )
+            pbar.replace_with(new_pbar)
+        return str(soup)
