9b3f520203b51dff774b723a91cf0e0c3b44cc5f
Magsen Chebini <magsen.che@gmail.com>
2024-04-01 17:07:34 +0200

    [refactor] custom logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -25,10 +25,9 @@ def custom(name):
         style="%",
     )
 
-    # Create a logger and set its formatter
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)  # Set your desired logging level
-    handler = logging.StreamHandler()  # You can also specify a different output stream
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
